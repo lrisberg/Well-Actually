6794de7e85850ed2eaa42d6601a07892591b7a0c
Lauren <lauren.risberg@gmail.com>
2017-05-21 11:55:50 -0600

    add server-side validations on title and links on erb files

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -1,8 +1,42 @@
 class SourcesController < ApplicationController
+  def index
+    @sources = Source.all
+  end
+
+  def show
+    @source = Source.find(params[:id])
+  end
+
+  # explicitly saying the sources/new route exists
   def new
+    @source = Source.new
   end
 
   def create
-    render plain: params[:source].inspect
+    @source = Source.new(source_params)
+
+    if @source.save
+      redirect_to @source
+    else
+      render 'new'
+    end
   end
+
+  private
+    def source_params
+      params.require(:source).permit(:user_id, :title, :link, :journal, :abstract, :notes, :wins)
+    end
 end
+
+
+
+# t.integer :user_id
+# t.text :title
+# t.text :link
+# t.string :journal
+# t.text :abstract
+# t.text :notes
+# t.integer :wins
+#
+#
+# :user_id, :title, :link, :journal, :abstract, :notes, :wins
--- app/models/source.rb
+++ app/models/source.rb
@@ -1,2 +1,6 @@
 class Source < ApplicationRecord
+  validates :title, presence: true
+  # validates :link, presence: true
+  # validates :abstract, presence: true
+  # validates :journal, presence: true
 end
