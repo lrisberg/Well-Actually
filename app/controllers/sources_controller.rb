2ba12a228c198605870f9a358a9f9f347e145e0e
Lauren <lauren.risberg@gmail.com>
2017-05-21 19:59:34 -0600

    remove out of date comments

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -28,7 +28,6 @@ class SourcesController < ApplicationController
     end
   end
 
-  # this is not returning the right response. wtf?
   def update
     @source = Source.find(params[:id])
 
@@ -50,16 +49,3 @@ class SourcesController < ApplicationController
       params.require(:source).permit(:id, :title, :link, :journal, :abstract, :notes)
     end
 end
-
-
-
-# t.integer :user_id
-# t.text :title
-# t.text :link
-# t.string :journal
-# t.text :abstract
-# t.text :notes
-# t.integer :wins
-#
-#
-# :user_id, :title, :link, :journal, :abstract, :notes, :wins
