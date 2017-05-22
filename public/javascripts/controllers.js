cd6aa26033623000d35579de8184ea4d8593c36b
Lauren <lauren.risberg@gmail.com>
2017-05-22 17:40:57 -0600

    first iteration of the Pontificator

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -26,6 +26,17 @@
       vm.toggleForm = function() {
         vm.showForm = !vm.showForm;
       }
+
+      vm.getSynonyms = function(word) {
+        $http.get(`http://words.bighugelabs.com/api/2/0d810a8b06d0aa825cc498c6f7805081/${word}/json`).then((response) => {
+          let data = response.data;
+          for (key in data) {
+            vm.synonyms = response.data[key].syn;
+          }
+          // vm.nounSyns = response.data.noun.syn;
+          // vm.verbSyns = response.data.verb.syn;
+        })
+      }
     }
 
     function WaSourceController() {
