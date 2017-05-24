d738452b9e77500593a9e889d4d941a942de3e76
144mdgross <144.mdgross@gmail.com>
2017-05-24 10:32:51 -0600

    fixes conflict in db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -23,10 +23,16 @@ ActiveRecord::Schema.define(version: 20170524021004) do
     t.text "abstract"
     t.text "notes"
     t.integer "wins"
-    t.integer "friends_lost"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "tags", default: [], array: true
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
