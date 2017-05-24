b1901a944e33cc040f692551cd462f2d862c2153
144mdgross <144.mdgross@gmail.com>
2017-05-24 10:46:01 -0600

    schema.db file change after migrating tables again

--- db/schema.rb
+++ db/schema.rb
@@ -23,16 +23,18 @@ ActiveRecord::Schema.define(version: 20170524021004) do
     t.text "abstract"
     t.text "notes"
     t.integer "wins"
+    t.integer "friends_lost"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "tags", default: [], array: true
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
+
 end
