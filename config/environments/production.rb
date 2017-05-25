aa01339649fd63c1904fa044c8973eca28a27d8b
Lauren <lauren.risberg@gmail.com>
2017-05-25 17:39:00 -0600

    require json_web_token on user controller

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,9 +76,6 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-
-  config.autoload_paths << Rails.root.join('lib')
-
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
